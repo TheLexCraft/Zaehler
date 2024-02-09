3b34d4be1cfa0145690d9019827456862d24966d
TheLexCraft <77213356+TheLexCraft@users.noreply.github.com>
2024-02-09 22:06:08 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,160 @@
 input.onButtonPressed(Button.A, function () {
-    zähler = zähler + 1
-    basic.showNumber(zähler)
+    if (state == 0) {
+        first += 1
+    }
+    if (state == 1) {
+        _symbol += 1
+        if (_symbol == 0) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+        }
+        if (_symbol == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (_symbol == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+        if (_symbol == 3) {
+            basic.showLeds(`
+                . . # . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . # . .
+                `)
+        }
+        if (_symbol > 3) {
+            _symbol = 0
+        }
+    }
+    if (state == 2) {
+        second += 1
+        basic.showNumber(second)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (state == 0) {
+        state = 1
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+    basic.pause(1000)
+    if (state == 1) {
+        state = 2
+    }
+    basic.pause(1000)
+    if (state == 2) {
+        state = 3
+    }
+    basic.pause(1000)
+    if (state == 3) {
+        first = 0
+        second = 0
+        state = 0
+        _symbol = 0
+    }
+    basic.pause(1000)
 })
 input.onButtonPressed(Button.B, function () {
-    zähler = zähler - 1
-    if (zähler == -1) {
-        zähler = 0
+    if (state == 0) {
+        first += -1
     }
-    basic.showNumber(zähler)
+    if (state == 1) {
+        _symbol += -1
+        if (_symbol == 0) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+        }
+        if (_symbol == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (_symbol == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+        if (_symbol == 3) {
+            basic.showLeds(`
+                . . # . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . # . .
+                `)
+        }
+        if (_symbol < 0) {
+            _symbol = 3
+        }
+    }
+    if (state == 2) {
+        second += -1
+        basic.showNumber(second)
+    }
 })
-let zähler = 0
-basic.showNumber(zähler)
+let _symbol = 0
+let state = 0
+let second = 0
+let first = 0
+first = 0
+second = 0
+state = 0
+_symbol = 0
+basic.forever(function () {
+    if (state == 0) {
+        basic.showNumber(first)
+    }
+    if (state == 2) {
+        basic.showNumber(second)
+    }
+    if (state == 3) {
+        if (_symbol == 0) {
+            basic.showNumber(first + second)
+        }
+        if (_symbol == 1) {
+            basic.showNumber(first - second)
+        }
+        if (_symbol == 2) {
+            basic.showNumber(first * second)
+        }
+        if (_symbol == 3) {
+            basic.showNumber(first / second)
+        }
+    }
+})
